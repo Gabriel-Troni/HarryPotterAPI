a35f08b9b14ae9aa3414df50ffe0c0ab33a9c275
Gabriel Troni <troni.gabriel.10@gmail.com>
2025-06-28 21:31:02 -0300

    add navigation

--- app/src/main/java/com/example/harrypotterapi/ui/dashboard/Dashboard.kt
+++ app/src/main/java/com/example/harrypotterapi/ui/dashboard/Dashboard.kt
@@ -1,11 +1,17 @@
 package com.example.harrypotterapi.ui.dashboard
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.harrypotterapi.R
+import com.example.harrypotterapi.ui.character.Character
+import com.example.harrypotterapi.ui.spells.Spell
+import com.example.harrypotterapi.ui.staff.Staff
+import com.example.harrypotterapi.ui.students.Students
 
 class Dashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,4 +24,29 @@ class Dashboard : AppCompatActivity() {
             insets
         }
     }
+
+    fun listCharacters(view: View) {
+        val intent = Intent(this, Character::class.java)
+        startActivity(intent)
+    }
+
+    fun listStaff(view: View) {
+        val intent = Intent(this, Staff::class.java)
+        startActivity(intent)
+    }
+
+    fun listStudents(view: View) {
+        val intent = Intent(this, Students::class.java)
+        startActivity(intent)
+    }
+
+    fun listSpells(view: View) {
+        val intent = Intent(this, Spell::class.java)
+        startActivity(intent)
+    }
+
+    fun exit(view: View) {
+        finish()
+    }
+
 }
